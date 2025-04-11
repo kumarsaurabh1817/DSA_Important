f931c3e22407b0a6e67698d43a84fdcde7e57eed
Saurabh Kumar <134209734+kumarsaurabh1817@users.noreply.github.com>
2025-04-11 13:34:46 +0000

    Typecasting

---
+++ C++_Basics/TypeCasting.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+int main() {
+    /*
+    int a = 5;
+    float b = 4.2;
+    // int answer = a + b;
+    int answer = b + a;
+    cout<<answer<<endl;
+    */
+    
+    /*
+    char ch = 'A';
+    // int a = ch + 1;
+    char a = ch + 1;
+    cout<<a<<endl;
+    return 0;
+    */
+    
+    /*
+    int a = 97;
+    char ch = a;
+    cout<<a<<endl;
+    cout<<ch<<endl;
+    */
+    
+    /*
+    char ch = 'B';
+    int value = ch;
+    cout<<value<<endl;
+    */
+    
+    /*
+    int a = 65;
+    float b = 4.2;
+    float answer = a + (int)(b);
+    // char answer = a + (int)(b);
+    cout<<answer<<endl;
+    */
+    
+    /*
+    double PI = 3.14159265;
+    int valuePI = (int)(PI);
+    cout<<valuePI<<endl;
+    */
+    
+    /*
+    float floatingNumber = 65.35;
+    char ch = (char)(floatingNumber);
+    cout<<ch<<endl;
+    */
+    
+    /*
+    int a = 10;
+    int b = 3.0;
+    // float c = (float)a / b;
+    int c = (float)a / b;
+    cout<<c<<endl;
+    */
+    
+}
